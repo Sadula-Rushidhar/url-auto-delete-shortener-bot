dfc028119a8bc61d55fd49dffec15fd4da95fc75
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2022-12-21 11:15:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,15 +29,15 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001589316857 -1001869924693 -1001787805183 -1001446583785').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5175000602').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '-1001589316857 -1001869924693')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001589316857 -1001869924693 -1001787805183 -1001446583785')
 auth_grp = environ.get('AUTH_GROUP', '-1001733020750')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rushidhar:rushidharr@rushiiiii.1ebtm6s.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "rushiiiii")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001875704370'))
